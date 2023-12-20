216be4f355e02ba4ed0c59b4254b53467842955e
moonymel <musumeci.me@gmail.com>
2023-12-20 15:36:18 +0100

    ESERCIZIO COMPLETATO

--- js/script.js
+++ js/script.js
@@ -3,15 +3,22 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
-
+            mails: [],
         }
     },
     
     created(){
-
+        this.generateMails();
+        console.log(this.mails)
     },
     
     methods: {
-
+        generateMails(){
+            for(let i=0; i<10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+                    this.mails.push(response.data.response);
+            })
+            }
+        }
     }
 }).mount('#app')
